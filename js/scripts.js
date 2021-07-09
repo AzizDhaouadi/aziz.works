1182ee69a20028817ada9650fae73e0574bbcef1
Aziz Dhaouadi <dhaziz.works@gmail.com>
2021-07-08 23:03:33 -0400

    Added the javascript responsible for terminal typing animation

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,27 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+var typed = new Typed('.typed', {
+  strings: [
+      "whoami<br/>" +
+      "><span class='caret'>$</span> azizdhaouadi<br/> ^100" +
+      "><span class='caret'>$</span> stat aziz.human<br/> ^100" +
+      "><span class='caret'>$</span> about: I am your friendly neighborhood Digital Marketer.<br/> ^100" +
+      "><span class='caret'>$</span> skills: SEO, SEM, Copywriting, Google Analytics and Co.<br/> ^100" +
+      "><span class='caret'>$</span> job: Marketing Specialist at <a href='http://www.trafficjunky.com/'>TrafficJunky.</a><br/> ^100" +
+      "><span class='caret'>$</span> interests: physics, software development, data analysis, and tennis.<br/> ^300" +
+      "><span class='caret'>$</span> highlights:  <a href='https://vocal.media/01/augmented-and-virtual-reality-leveraging-and-monetizing-the-technologies'>Monetizing AR and VR</a>, <a href='https://www.isanet.org/Conferences/Toronto-2019'>Speaker at the ISA 2019 Convention.</a><br/>" +
+      "><span class='caret'>$</span>"
+      /*
+      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
+    ],
+    showCursor: true,
+    cursorChar: '_',
+        autoInsertCss: true,
+        typeSpeed: 15,
+        startDelay: 50,
+        loop: false,
+        showCursor: false
+  });
+  $('.message form').hide()
